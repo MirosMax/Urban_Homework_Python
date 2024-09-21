cffb95d87bbbe9620f5fbc78fff41e007335cb83
Max Miroshnichenko <foxmax@list.ru>
2024-09-21 08:16:22 +0300

    Corrected the brackets

--- Module 3/module_3_2_mailing_list.py
+++ Module 3/module_3_2_mailing_list.py
@@ -6,12 +6,12 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
             not recipient.endswith('.com')
             and not recipient.endswith('.ru')
             and not recipient.endswith('.net')
-    )
+        )
         or (
             not sender.endswith('.com')
             and not sender.endswith('.ru')
             and not sender.endswith('.net')
-    )
+        )
     ):
         print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
     elif sender == recipient:
