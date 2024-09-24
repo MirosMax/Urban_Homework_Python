805a49fdb4dc949dfe85c27c3de72b2cb6c99f98
Max Miroshnichenko <foxmax@list.ru>
2024-09-24 19:42:17 +0300

    Рабочий вариант, учитывающий нули на конце

--- Module 3/module_3_5_recursia.py
+++ Module 3/module_3_5_recursia.py
@@ -2,7 +2,11 @@ def get_multiplied_digits(number):
     str_number = str(number)
     if len(str_number) > 1:
         first = int(str_number[0])
+        if first == 0:
+            first = 1
         return first * get_multiplied_digits(int(str_number[1:]))
+    elif number == 0:
+        return 1
     else:
         return number
 
