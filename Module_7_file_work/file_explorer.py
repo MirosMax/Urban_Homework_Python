903a9021e440ad499e21e103417bb7ee7ed47054
Max Miroshnichenko <foxmax@list.ru>
2024-11-08 23:25:48 +0300

    Initial

--- Module_7_file_work/file_explorer.py
+++ Module_7_file_work/file_explorer.py
@@ -1,15 +1,28 @@
 import tkinter as tk
+import os
+from tkinter import filedialog
+
+
+def file_select():
+    filename = filedialog.askopenfilename(initialdir='.', title='Выберите файл',
+                                          filetypes=(('Текстовый файл', '.txt'),
+                                                        ('Файл Python', '.py'),
+                                                        ('Все файлы', '*')))
+
+    text['text'] += '\n' + filename
+    os.startfile(filename)
+
 
 window = tk.Tk()
 window.title('Проводник')
-window.geometry('450x500')
+window.geometry('550x150')
 window.resizable(False, False)
 window.configure(bg='white')
 
-text = tk.Label(window, text='Файл:', width=15, height=5, background='#C9D5EE')
+text = tk.Label(window, text='Файл:', width=80, height=5, background='#C9D5EE', foreground='#00308F')
 text.grid(column=1, row=1)
 
-button_select = tk.Button(window, text='Выбрать файл', width=15, height=2)
-button_select.grid(column=1, row=2)
-
+button_select = tk.Button(window, text='Выбрать файл', width=15, height=2, background='#C9D5EE',
+                          foreground='#00308F', command=file_select)
+button_select.grid(column=1, row=2, pady=5)
 window.mainloop()
