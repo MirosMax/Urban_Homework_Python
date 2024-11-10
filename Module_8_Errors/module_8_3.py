0106629c59ced64ed29210e6fc132150765ec3c2
Max Miroshnichenko <foxmax@list.ru>
2024-11-10 19:29:49 +0300

    Initial

---
+++ Module_8_Errors/module_8_3.py
@@ -0,0 +1,61 @@
+class Car:
+    def __init__(self, model: str, vin_number: int, number: str):
+        self.model = model
+        self.__vin = vin_number
+        self.__numbers = number
+        self.__is_valid_vin(vin_number)
+        self.__is_valid_numbers(number)
+
+    def __is_valid_vin(self, vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип vin номер')
+        elif vin_number < 1000000 or vin_number > 9999999:
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        else:
+            return True
+
+    def __is_valid_numbers(self, number):
+        if not isinstance(number, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        elif len(number) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
+        else:
+            return True
+
+
+class IncorrectVinNumber(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+try:
+    first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{first.model} успешно создан')
+
+try:
+    second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{second.model} успешно создан')
+
+try:
+    third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+    print(exc.message)
+except IncorrectCarNumbers as exc:
+    print(exc.message)
+else:
+    print(f'{third.model} успешно создан')
