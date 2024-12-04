e046678f99ada5a1ca67e4720422e057c76b0d31
Max Miroshnichenko <foxmax@list.ru>
2024-12-04 19:03:44 +0300

    Initial

---
+++ Module_10_Threads/module_10_practice_1_1.py
@@ -0,0 +1,54 @@
+'''
+Симуляция общепита.
+Приготовление булок и котлет. Первый сотрудник бесконечно жарит булки, второй готовит котлеты. В 20% случаев булки будут
+подгарать.
+'''
+
+
+
+import time
+from threading import Thread
+import random
+import queue
+
+
+class Bulka(Thread):
+    def __init__(self, queue):
+        super().__init__()
+        self.queue = queue
+
+    def run(self):
+        while True:
+            time.sleep(1)
+            if random.random() > 0.8:
+                self.queue.put('подгорелая булка')
+            else:
+                self.queue.put('хорошая булка')
+
+
+class Kotleta(Thread):
+    def __init__(self, queue, count):
+        super().__init__()
+        self.queue = queue
+        self.count = count
+
+    def run(self):
+        while self.count:
+            print(self.queue.qsize())
+            bulka = self.queue.get()
+            if bulka == 'хорошая булка':
+                time.sleep(random.randint(1, 3))
+                self.count -= 1
+                print('Бургер готов!')
+            print(f'Булок в запасе: {self.count}')
+
+
+q = queue.Queue(maxsize=20)
+
+t1 = Bulka(q)
+t2 = Kotleta(q, 20)
+
+t1.start()
+t2.start()
+t1.join()
+t2.join()
